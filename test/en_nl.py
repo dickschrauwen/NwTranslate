748cf3b6ffd7a18241021b38048fd3084ae5a9b7
Dick Schrauwen <dickschrauwen@gmail.com>
2018-02-22 12:56:22 +0100

    Create en_nl.py

---
+++ test/en_nl.py
@@ -0,0 +1,14 @@
+# en_nl.py
+
+from collections import OrderedDict
+td = OrderedDict([
+('''<default>''', '''<target>'''),
+('''to bad''','''jammer'''),
+('''ugly''','''lelijk'''),
+('''tree''','''boom'''),
+('''branch''','''tak'''),
+# ('''Python''','''Python'''),
+# ('''python''','''python'''),
+('''AI''','''kunstmatige intelligentie'''),
+
+])
